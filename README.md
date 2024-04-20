17f05750e8cfa45cfcde0888629702006fff5de3
Marcel-MSC <marcelomarcos.s.c@gmail.com>
2024-04-20 16:30:55 -0300

    Add logo, readme, change messages of return to post`s, tested everything with Insomnia

---
+++ README.md
@@ -0,0 +1,69 @@
+<p align="center">
+  <a href="" rel="noopener">
+ <img width=200px height=200px src="./imageReadme/logo.jpg" alt="Project logo"></a>
+</p>
+
+<h3 align="center">auth-server</h3>
+
+<div align="center">
+
+[![Status](https://img.shields.io/badge/status-active-success.svg)]()
+[![GitHub Issues](https://img.shields.io/github/issues/kylelobo/The-Documentation-Compendium.svg)](https://github.com/kylelobo/The-Documentation-Compendium/issues)
+[![GitHub Pull Requests](https://img.shields.io/github/issues-pr/kylelobo/The-Documentation-Compendium.svg)](https://github.com/kylelobo/The-Documentation-Compendium/pulls)
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](/LICENSE)
+
+</div>
+
+---
+
+<p align="center"> This is a project to learn about JWT, server authentication, creat user, verify user token and account email.
+    <br> 
+</p>
+
+## 📝 Table of Contents
+
+- [About](#about)
+- [Getting Started](#getting_started)
+- [Usage](#usage)
+- [Authors](#authors)
+- [Acknowledgments](#acknowledgement)
+
+## 🧐 About <a name = "about"></a>
+
+This project use jsonwebtoken, bcryptjs, cors, express, lowdb to create a server that can:
+- add user
+- check if is loggin
+- verify user
+- account email verification
+
+## 🏁 Getting Started <a name = "getting_started"></a>
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+- clone repository
+- create a jwt secret key on .env
+- npm i
+- npm run dev
+
+
+### Installing
+
+```
+npm i
+```
+
+And run
+
+```
+npm run dev
+```
+
+## 🎈 Usage <a name="usage"></a>
+
+You can use this auth server to authentication of users.
+You can use as a API or developer more features.
+Using a app to created the post`s. I use Insommia.
+
+## ✍️ Authors <a name = "authors"></a>
+
+- [@Marcel-MSC](https://github.com/Marcel-MSC) - Idea & Initial work
--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import bcrypt from 'bcryptjs'
 import cors from 'cors'
 import jwt from "jsonwebtoken"
 import { JSONFilePreset } from 'lowdb/node'
+import 'dotenv/config'
+
 const defaultData = { users: [] }
 let db
 async function startLowdb () {
@@ -13,7 +15,7 @@ startLowdb()
 // Initialize Express app
 const app = express()
 // Define a JWT secret key. This should be isolated by using env letiables for security
-const jwtSecretKey = "dsfdsfsdfdsvcsvdfgefg"
+const jwtSecretKey = process.env.JWT_SECRET_KEY
 
 // Set up CORS and JSON middlewares
 app.use(cors())
@@ -22,7 +24,7 @@ app.use(express.urlencoded({ extended: true }))
 
 // Basic home route for the API
 app.get("/", (_req, res) => {
-    res.send("Auth API.\nPlease use POST /auth & POST /verify for authentication")
+    res.send("Auth API.\nPlease use POST /auth & POST /verify for authentication");
 })
 
 // The auth endpoint that creates a new user record or logs a user based on an existing record
@@ -47,7 +49,7 @@ app.post("/auth", (req, res) => {
                     signInTime: Date.now(),
                 }
                 const token = jwt.sign(loginData, jwtSecretKey)
-                res.status(200).json({ message: "success", token })
+                res.status(200).json({ message: "authentication success", token })
             }
         })
         // If no user is found, hash the given password and create a new entry in the auth db with the email and hashed password
@@ -71,18 +73,13 @@ app.post("/auth", (req, res) => {
 
 // The verify endpoint that checks if a given JWT token is valid
 app.post('/verify', (req, res) => {
-    const tokenHeaderKey = "jwt-token"
-    const authToken = req.headers[tokenHeaderKey]
+    //const tokenHeaderKey = "jwt-token"
+    const authToken = req.headers.tokenheaderkey;
+    console.log('req', req.headers)
+    debugger
     try {
       const verified = jwt.verify(authToken, jwtSecretKey)
-      if (verified) {
-        return res
-          .status(200)
-          .json({ status: "logged in", message: "success" })
-      } else {
-        // Access Denied
-        return res.status(401).json({ status: "invalid auth", message: "error" })
-      }
+      return verified ? res.status(200).json({ status: "logged in", message: "verify with success" }) : res.status(401).json({ status: "invalid auth", message: "error" });
     } catch (error) {
       // Access Denied
       return res.status(401).json({ status: "invalid auth", message: "error" })
@@ -90,7 +87,6 @@ app.post('/verify', (req, res) => {
 })
 
 // An endpoint to see if there's an existing account for a given email address
-
 app.post('/check-account', (req, res) => {
     const { email } = req.body
     console.log(req.body)
